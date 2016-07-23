9e7b9fd73c7a1230b337a453ba2bbb01b415af71
Pwl Webdev <pwl.webdev@gmail.com>
2016-07-23 13:46:34 +0200

    migration to microposts added

--- db/migrate/20160723102815_add_picture_to_microposts.rb
+++ db/migrate/20160723102815_add_picture_to_microposts.rb
@@ -1,5 +1,9 @@
 class AddPictureToMicroposts < ActiveRecord::Migration[5.0]
   def change
+  	create_table :microposts do |t|
+  		t.text :content
+  		t.integer :user_id
+  	end
     add_column :microposts, :picture, :string
   end
 end
